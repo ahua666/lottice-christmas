f6748522ba9ecb7996ebec650f9d55f157478e3d
Star <2524329629@qq.com>
2022-12-18 23:10:59 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Lottie from './components/lottie.vue'
+
+</script>
+
+<template>
+  <Lottie width="100%" height="100%" style="background-color: #000000" ></Lottie>
+
+  <!--  <header>-->
+<!--    &lt;!&ndash;    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />&ndash;&gt;-->
+
+<!--    <div class="wrapper">-->
+<!--      &lt;!&ndash;      <HelloWorld msg="You did it!" />&ndash;&gt;-->
+<!--    </div>-->
+<!--  </header>-->
+
+<!--  <main>-->
+<!--    &lt;!&ndash;    <TheWelcome />&ndash;&gt;-->
+<!--  </main>-->
+</template>
+
+<style scoped>
+
+/*header {*/
+/*  line-height: 1.5;*/
+/*}*/
+
+/*.logo {*/
+/*  display: block;*/
+/*  margin: 0 auto 2rem;*/
+/*}*/
+
+/*@media (min-width: 1024px) {*/
+/*  header {*/
+/*    display: flex;*/
+/*    place-items: center;*/
+/*    padding-right: calc(var(--section-gap) / 2);*/
+/*  }*/
+
+/*  .logo {*/
+/*    margin: 0 2rem 0 0;*/
+/*  }*/
+
+/*  header .wrapper {*/
+/*    display: flex;*/
+/*    place-items: flex-start;*/
+/*    flex-wrap: wrap;*/
+/*  }*/
+/*}*/
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import lottie from 'lottie-web'
+// import './assets/main.css'
+
+createApp(App).mount('#app')
